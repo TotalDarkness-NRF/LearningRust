3175d49d31b4c477b26b8a72902f3cf10b9e4d6b
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2021-06-29 16:26:27 -0400

    Add user input

--- fractorial/src/main.rs
+++ fractorial/src/main.rs
@@ -1,11 +1,23 @@
 #![allow(non_snake_case)] // stop warning for not using snake case
+use std::io;
 fn main() {
-    println!("{}", fractorial(10));
+    println!("Insert a number to calculate the fractorial");
+    let mut number =String::new();
+
+    io::stdin()
+    .read_line(&mut number)
+    .expect("Failed to read line");
+
+    let number: u32 = number.trim().parse().expect("Please type a number!");
+
+    println!("The fractorial of {0} is {1}", number, fractorial(number));
 }
 
 fn fractorial(n: u32) -> u32 {
     fractorialRecursive(1, n)
 }
+
+// I would normally use function overloading here but that is not a good idea for rust.
 
 fn fractorialRecursive(x: u32, n: u32) -> u32 {
     if x >= n || x < 1 {return x};

b151d3276ff20e437556b826e26fb68d672c53a1
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2021-06-30 00:01:27 -0400

    Add notes on loops

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     variables();
     ifelse();
     compoundTypes();
+    loops();
 }
 
 fn variables() {
@@ -57,3 +58,30 @@ Paramenters are declared in the brackets using variable: type.
 Return values are specified using -> type after the brackets and before the {}
 A final return does not require a semi colon. Use this if its the final return
  */
+
+ fn loops() {
+     // The infinite loop
+     loop {
+         break; // use this to break out of the loop
+     }
+     // The while loop.
+     let mut condition: bool = true;
+     while condition { // Keep looping until conditon is not true
+         condition = false;
+    }
+    // Loops can be used as expressions
+    let mut counter = 0;
+    let result = loop { 
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {}", result);
+
+    let a = [10, 20, 30, 40, 50];
+    // For loops are more applicable and can easily go through a collection
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+ }

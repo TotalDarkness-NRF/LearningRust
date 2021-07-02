8fa85e69456d919e98f232987da75427ca7be056
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2021-07-02 03:05:30 -0400

    Add notes on structure debugging

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -195,14 +195,28 @@ fn structs() {
         "{}, level {}. Remote: {}. Grades: {}, {}, {}, {}. Average: {}",
         student2.name, student2.level, student2.remote, marks2.0, marks2.1, marks2.2, marks2.3, marks2.4
     );
+
+    /*
+    These println are long and dumb. There is a way to make it easier. 
+    Before the struct add #[derive(Debug)]
+    In print use {:?} or {:#?} for pretty-print
+    */
+    println!("Normal debug print");
+    println!("student1 is {0:?}, marks1 is {1:?}", student1, marks1);
+    println!("student2 is {0:?}, marks2 is {1:?}", student2, marks2);
+    println!("Pretty debug print");
+    println!("student1 is {0:#?}, marks1 is {1:#?}", student1, marks1);
+    println!("student2 is {0:#?}, marks2 is {1:#?}", student2, marks2);
 }
 // A clasic struct
+#[derive(Debug)]
 struct Student {
     name: String,
     level: u8,
     remote: bool,
 }
 // A tuple struct
+#[derive(Debug)]
 struct Grades(char, char, char, char, f32);
 // Unit struct
 //struct Unit;

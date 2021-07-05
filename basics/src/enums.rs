389a3da0070eb4b1df360c86063aaa19c50131f2
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2021-07-05 01:03:38 -0400

    Add notes on match and if let

--- basics/src/enums.rs
+++ basics/src/enums.rs
@@ -24,6 +24,34 @@ impl ToString for IpAddress {
     }
 }
 
+enum Coin {
+    _Penny,
+    _Nickel,
+    _Dime,
+    _Quarter,
+}
+
+impl Coin {
+    fn value(&self ) -> u8 {
+        match self {
+            Coin::_Penny => 1,
+            Coin::_Nickel => 5,
+            Coin::_Dime => 10,
+            Coin::_Quarter => 25,
+        }
+    }
+}
+
+/*
+ Use of option for no values or a value.
+ Shows that matches are exhaustive and all cases must be handled
+*/
+fn plusOne(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
 // We can implement this with either a struct of simply implementing it in an enum
 
 pub fn enums() {
@@ -44,4 +72,35 @@ pub fn enums() {
 
     println!("{}", home.to_string());
     println!("{}", loopback.to_string());
+
+    let coin:Coin = Coin::_Nickel;
+    println!("The coin is worth {}", coin.value());
+
+    let five = Some(5);
+    let six = plusOne(five);
+    let none = plusOne(None);
+    println!("{:?} {:?}", six, none);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (), // A placeholder if we do not care about any other value
+    }
+
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    // An if let allows us to to perform a quick match like above easier
+    let some_u8_value = Some(0u8);
+    if let Some(3) = some_u8_value {
+        println!("three");
+    } else {
+        println!("Dont care for that value");
+    }
 }

e332f2df6a55feaed9762ed09ee0deedeb89c48e
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2021-07-09 04:27:28 -0400

    Add general controls

---
+++ terminalGame/src/controls.rs
@@ -0,0 +1,29 @@
+use termion::event::Key;
+
+pub struct Controls {
+    pub quit: Key,
+    pub up: Key,
+    pub left: Key,
+    pub down: Key,
+    pub right: Key,
+    pub attackUp: Key,
+    pub attackLeft: Key,
+    pub attackDown: Key,
+    pub attackRight: Key,
+}
+
+impl Controls {
+    pub fn new() -> Self {
+        Controls {
+            quit: Key::Char('q'),
+            up: Key::Char('w'),
+            left: Key::Char('a'),
+            down: Key::Char('s'),
+            right: Key::Char('d'),
+            attackUp: Key::Ctrl('w'),
+            attackLeft: Key::Ctrl('a'),
+            attackDown: Key::Ctrl('s'),
+            attackRight: Key::Ctrl('d'), 
+        }
+    }
+}
--- terminalGame/src/game.rs
+++ terminalGame/src/game.rs
@@ -1,85 +1,84 @@
-use std::io::{stdin, Write};
+use std::{io::stdin, process::exit};
 
 use rand::{prelude::ThreadRng, thread_rng};
-use termion::{clear, color, cursor, event::Key, input::TermRead, screen};
+use termion::{color, event::Key, input::TermRead};
 
-use crate::{character::Character, position::{Position, Direction}, terminal::{Terminal, getBoundaries}};
+use crate::{
+    character::Character,
+    controls::Controls,
+    player::Player,
+    position::{Direction, Position},
+    terminal::{getBoundaries, Terminal},
+};
 
 pub struct Game {
-    character: Character,
+    character: Player,
     points: u16,
     terminal: Terminal,
+    controls: Controls,
     rng: ThreadRng,
 }
 
 impl Game {
     pub fn new() -> Self {
         Game {
-            character: Character::create(Position::newOrigin(), Box::new(color::LightRed)),
+            character: Player::create(Position::newOrigin(), Box::new(color::LightRed)),
             points: 0,
             terminal: Terminal::getRaw(),
+            controls: Controls::new(),
             rng: thread_rng(),
         }
     }
 
     pub fn start(&mut self) {
         let stdin = stdin();
-        self.terminal.write( format!("{}{}{}", screen::ToAlternateScreen, clear::All, cursor::Hide));
+        self.terminal.begin();
         // TODO the game only updates during key inputs
-        for c in stdin.keys() {
+        for key in stdin.keys() {
             self.terminal.write(format!(
                 "{}{}",
                 termion::cursor::Goto(1, 1),
                 termion::clear::CurrentLine
             ));
-
-            match c.unwrap() {
-                /* 
-                Key::Char('q') => break,
-                Key::Char(c) => println!("{}", c),
-                Key::Alt(c) => println!("^{}", c),
-                Key::Ctrl(c) => println!("*{}", c),
-                Key::Esc => println!("ESC"),
-                Key::Left => println!("←"),
-                Key::Right => println!("→"),
-                Key::Up => println!("↑"),
-                Key::Down => println!("↓"),
-                Key::Backspace => println!("×"),
-                */
-                Key::Char('q') => {self.quit(); break},
-                Key::Char('w') => self.character.moves(&mut self.terminal, Direction::Up),
-                Key::Char('a') => self.character.moves(&mut self.terminal, Direction::Right),
-                Key::Char('s') => self.character.moves(&mut self.terminal, Direction::Down),
-                Key::Char('d') => self.character.moves(&mut self.terminal, Direction::Left),
-                Key::Ctrl('w') => {
-                    let position: Position = Position::copy(self.character.getPosition());
-                    self.character.getWeapon().shoot(&mut self.terminal, Direction::Up, position);
-                },
-                Key::Ctrl('a') => {
-                    let position: Position = Position::copy(self.character.getPosition());
-                    self.character.getWeapon().shoot(&mut self.terminal, Direction::Right, position);
-                },
-                Key::Ctrl('s') => {
-                    let position: Position = Position::copy(self.character.getPosition());
-                    self.character.getWeapon().shoot(&mut self.terminal, Direction::Down, position);
-                },
-                Key::Ctrl('d') => {
-                    let position: Position = Position::copy(self.character.getPosition());
-                    self.character.getWeapon().shoot(&mut self.terminal, Direction::Left, position);
-                },
-                _ => {}
-            }
+            self.handleKey(key.unwrap());
 
             let bound = getBoundaries();
-            self.terminal.write(format!("{} {} {}",
-            bound.getX(), bound.getY(), self.character.to_string()));
+            self.terminal.write(format!(
+                "{} {} {}",
+                bound.getX(),
+                bound.getY(),
+                self.character.to_string()
+            ));
             self.character.update(&mut self.terminal);
-            self.terminal.terminal.flush().unwrap();
+            self.terminal.flush();
+        }
+    }
+
+    fn handleKey(&mut self, key: Key) {
+        let controls: &Controls = &self.controls;
+        if key == controls.quit {
+            self.quit();
+        } else if key == controls.up {
+            self.character.moves(&mut self.terminal, Direction::Up);
+        } else if key == controls.left {
+            self.character.moves(&mut self.terminal, Direction::Left);
+        } else if key == controls.down {
+            self.character.moves(&mut self.terminal, Direction::Down);
+        } else if key == controls.right {
+            self.character.moves(&mut self.terminal, Direction::Right);
+        } else if key == controls.attackUp {
+            self.character.attack(&mut self.terminal, Direction::Up);
+        } else if key == controls.attackLeft {
+            self.character.attack(&mut self.terminal, Direction::Left);
+        } else if key == controls.attackDown {
+            self.character.attack(&mut self.terminal, Direction::Down);
+        } else if key == controls.attackRight {
+            self.character.attack(&mut self.terminal, Direction::Right);
         }
     }
 
     fn quit(&mut self) {
-        self.terminal.write(format!("{}{}", cursor::Show, screen::ToMainScreen));
-        self.terminal.terminal.suspend_raw_mode().unwrap();
+        self.terminal.exit();
+        exit(0);
     }
 }

f0ea48a3b320bb3d9bf6d19af6c0660ce1ce0acd
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2021-07-10 01:06:42 -0400

    Add enemy types and defualt enemies

--- terminalGame/src/enemy.rs
+++ terminalGame/src/enemy.rs
@@ -1,9 +1,10 @@
-use termion::color::Color;
 use crate::{character::Character, position::{Direction, Position}, terminal::Terminal, weapon::Weapon};
+use rand::{Rng, thread_rng};
+use termion::color::{self, Color};
 
 pub struct Enemy {
-    position: Position,
     health: u8,
+    position: Position,
     weapon: Weapon,
     color: Box<dyn Color>,
 }
@@ -14,7 +15,7 @@ impl Character for Enemy {
     }
 
     fn update(&mut self, terminal: &mut Terminal) {
-        todo!()
+        
     }
 
     fn draw(&self, terminal: &mut Terminal) {
@@ -35,5 +36,60 @@ impl Character for Enemy {
 
     fn attack(&mut self, direction: Direction) {
         todo!()
+    }
+}
+
+impl Enemy {
+    pub fn new() -> Self {
+        let rng = thread_rng().gen_range(0.0..=100.0);
+        if rng < 50.0 { EnemyType::human() } 
+        else if rng < 75.0 { EnemyType::elf() }
+        else if rng < 95.0 { EnemyType::machine() }
+        else { EnemyType::alien() }
+    }
+}
+
+enum EnemyType {
+    Alien,
+    Elf,
+    Human,
+    Machine,
+}
+
+impl EnemyType {
+    pub fn human() -> Enemy {
+        Enemy {
+            health: 20,
+            position: Position::newRandom(),
+            weapon: Weapon::createShotgun(),
+            color: Box::new(color::Yellow),
+        }
+    }
+
+    pub fn alien() -> Enemy {
+        Enemy {
+            health: 30,
+            position: Position::newRandom(),
+            weapon: Weapon::createPistol(),
+            color: Box::new(color::LightGreen),
+        }
+    }
+
+    pub fn elf() -> Enemy {
+        Enemy {
+            health: 30,
+            position: Position::newRandom(),
+            weapon: Weapon::createBow(),
+            color: Box::new(color::Blue),
+        }
+    }
+    
+    pub fn machine() -> Enemy {
+        Enemy {
+            health: 20,
+            position: Position::newRandom(),
+            weapon: Weapon::createShotgun(),
+            color: Box::new(color::AnsiValue::grayscale(5)),
+        }
     }
 }
